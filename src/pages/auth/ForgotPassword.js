98f5976acb06ed45774685738c160319815e350f
RoseKimball <mrkimbal@usc.edu>
2020-11-19 21:05:21 -0800

    redirect user

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
@@ -6,6 +6,14 @@ import { useSelector } from 'react-redux';
 const ForgotPassword = ({ history }) => {
     const [email, setEmail] = useState('');
     const [loading, setLoading] = useState(false);
+
+    const { user } = useSelector((state) => ({ ...state }))
+
+    useEffect(() => {
+        if(user && user.token) {
+            history.push('/');
+        }
+    }, [user])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
